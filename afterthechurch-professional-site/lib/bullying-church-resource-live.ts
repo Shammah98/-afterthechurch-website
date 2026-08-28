ab1aab3631f84eb73383b7fa02d8f79fea998198
Shammah98 <73389252+Shammah98@users.noreply.github.com>
2026-08-28 14:59:54 +0200

    Use uploaded photo for bullying article

---
+++ afterthechurch-professional-site/lib/bullying-church-resource-live.ts
@@ -0,0 +1,8 @@
+import { bullyingChurchResource as baseResource } from "./bullying-church-resource";
+
+export const bullyingChurchResource = {
+  ...baseResource,
+  image: "/images/bullies-and-sissies-cover.jpg",
+  imageAlt:
+    "Black-and-white photograph of a seated student being surrounded and pointed at by other students in a school corridor."
+};
